56b3819bf158b9d243e7f4e5637f9f72b99d7403
Hakop <zlodeh@gmail.com>
2017-01-23 16:54:02 +0200

    Add files via upload

--- xnad0.py
+++ xnad0.py
@@ -1,5 +1,6 @@
-#import random  Пока думаю как с помощью рендома сделать игру компьютера с человеком.
+import random
 board = {1:"1", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9"}
+b = []
 def proverka():
     while True:
         cifra = input()
@@ -11,7 +12,14 @@ def proverka():
                 print("Вы вели число с больше значением, чем надо. Повоторите пожалуйста")
                 return proverka()
             else:
-                return int(cifra)
+                try:
+                    b.index(int(cifra))
+                    print("Данная клетка занята. Повторите пожалуйста")
+                    return proverka()
+                except ValueError:
+                    print("Вы вели правильно")
+                    b.append(int(cifra))
+                    return int(cifra)
         except ValueError:
             print("Вы вели букву. Ведите число")
 
@@ -54,12 +62,12 @@ def draw_board():
     elif board[1] == board[5] == board[9]:
         print("Вы виграли ")
         raise SystemExit
-
+    elif board[7] == board[5] == board[3]:
+        print("Вы виграли ")
+        raise SystemExit
 print("Добро пожаловать в игру Х и О")
 draw_board()
 print("Для того, чтоб ходить, вам надо вести чилос, куда вы должны поставить X или 0")
-
-
 for i in range(10):
     print("Ходить первый игрок X")
     first = proverka()
@@ -67,5 +75,5 @@ for i in range(10):
     print("Ходтив второй игрок 0")
     second = proverka()
     playy(second)
-#Пока не получается чделать так, чтоб если 1 игрок вел, то второй если выберает такую же цифру, то у второго написало отказ. А то перезаписывает веденные раньше цифры
+
 
